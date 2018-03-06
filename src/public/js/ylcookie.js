12de07e0232fd5547f1e6d363ee9361e6b8c2c49
袁理开 <1091372693@qq.com>
2018-03-06 11:16:49 +0800

    amend file
    amend file

---
+++ src/public/js/ylcookie.js
@@ -0,0 +1,38 @@
+/**
+ * Created by 袁理开 on 2018/3/6.
+ */
+/**
+ * Created by 袁理开 on 2018/1/13.
+ */
+var kie = function () {
+    var u = undefined;
+    var d = document;
+    this.cookie = function (name, value, str) {
+        if (value != u) {
+            if (str != u) {
+                var exp = new Date();
+                exp.setTime(exp.getTime() + str.expires * 24 * 60 * 60 * 1000);
+                d.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value) + ';expires=' + exp.toGMTString();
+            } else {
+                d.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+            }
+        } else {  //获取cookie
+            var str = d.cookie.split('; ');
+            for (var i = 0; i < str.length; i++) {
+                var er = str[i].split('=');
+                if (er[0] == decodeURIComponent(name)) {
+                    return decodeURIComponent(er[1])
+                }
+            }
+        }
+    };
+};
+var yl = new kie();
+kie.prototype.removeCookie=function(name){
+    yl.cookie(name,'',{
+        expires:-1
+    })
+};
+/**
+ * Created by 袁理开 on 2018/1/13.
+ */
